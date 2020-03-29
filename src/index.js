d6f878cf528b5d1c9a9f2551ef3d2d5bc1fc509a
Léu Almeida <leo@webid.net.br>
2020-03-29 19:54:14 -0300

    feat(src/index.js): create App iniital structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
+// import { NavigationContainer } from '@react-navigation/native';
 
-export default function src() {
-  return <div />;
+// import Routes from './routes'
+
+export default function App() {
+  return (
+    <>
+      {/* <NavigationContainer> */}
+      {/* <> */}
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      {/* </> */}
+      {/* </NavigationContainer> */}
+    </>
+  );
 }
