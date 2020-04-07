84cdd3b57ec3f8667faa32056be7a40ba7b21739
Léu Almeida <leo@webid.net.br>
2020-04-07 12:22:43 -0300

    chore(src/index): improving Redux use

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import 'react-native-gesture-handler';
-
 import React from 'react';
 import { StatusBar } from 'react-native';
-
-import './config/ReactotronConfig';
+import { Provider } from 'react-redux';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import Routes from './routes';
+import '~/config/ReactotronConfig';
+
+import { store, persistor } from '~/store';
+
+import App from './App';
 
 export default function Index() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar backgroundColor="#7D40E7" barStyle="light-content" />
+          <App />
+        </PersistGate>
+      </Provider>
     </NavigationContainer>
   );
 }
