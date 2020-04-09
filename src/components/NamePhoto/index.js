7c3327a94d10dfb7e5d8a34851a379e795a877a8
Léu Almeida <leo@webid.net.br>
2020-04-09 13:05:44 -0300

    feat(components/NamePhoto): introducing component structure

--- src/components/NamePhoto/index.js
+++ src/components/NamePhoto/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import PropTypes from 'prop-types';
 
-export default function NamePhoto() {
-  return <View />;
+import { Container, TextPhoto } from './styles';
+
+export default function NamePhoto({ name, ...rest }) {
+  const nameSplit = name?.split(' ');
+
+  const number = Math.floor(Math.random() * (5 + 1));
+
+  return (
+    <Container number={number} {...rest}>
+      <TextPhoto number={number}>
+        {nameSplit?.[0]?.[0]}
+        {nameSplit?.[1]?.[0]}
+      </TextPhoto>
+    </Container>
+  );
 }
+
+NamePhoto.propTypes = {
+  name: PropTypes.string.isRequired,
+};
